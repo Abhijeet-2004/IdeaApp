ce91fb33bbeca54bd085d7ab27231bfffbd56167
Abhijeet sahoo <124451670+Abhijeet-2004@users.noreply.github.com>
2023-07-02 00:24:59 +0530

    server config added

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
-
+const ServerConfig=require('./configs/server.config');
 const app=express();
 
-app.listen(5555,()=>
-console.log("Server started")
-)
+app.listen(ServerConfig.PORT,()=>{
+console.log ('Server started on the port number '+ServerConfig.PORT);
+})
